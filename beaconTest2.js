d65371c603c109b85ed34bcd038de6b0a5b4bbf7
ben <ben@invisibleflock.com>
2017-10-02 14:28:00 +0100

    Update beaconTest2.js

--- beaconTest2.js
+++ beaconTest2.js
@@ -1,6 +1,6 @@
 var beacon  = require('bleacon');
 console.log('hello there');
-var uuid = ""d0611e78bbb44591a5f8487910ae4366"";
+var uuid = "d0611e78bbb44591a5f8487910ae4366";
 beacon.startScanning(uuid, 0, 0);
 
 beacon.on('discover', function(bleacon){
