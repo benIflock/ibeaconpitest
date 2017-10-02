4d9733d3d3dc376bb85c55401204bc234aebd391
ben <ben@invisibleflock.com>
2017-10-02 14:28:44 +0100

    Delete beaconTest2.js

--- beaconTest2.js
+++
@@ -1,9 +0,0 @@
-var beacon  = require('bleacon');
-console.log('hello there');
-var uuid = "d0611e78bbb44591a5f8487910ae4366";
-beacon.startScanning(uuid, 0, 0);
-
-beacon.on('discover', function(bleacon){
-  console.log('got one');
-console.log(bleacon.uuid);
-});
