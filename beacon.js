b86c52dd66a4b46d8988b65f96b99555610fc7a4
ben <ben@invisibleflock.com>
2017-10-02 14:41:46 +0100

    Update beacon.js

--- beacon.js
+++ beacon.js
@@ -1,6 +1,10 @@
 var Bleacon = require('bleacon');
+var socket = require('socket.io-client')('http://localhost/scanner');
+
 Bleacon.startScanning();
 Bleacon.on('discover', function (bleacon) {
     console.log(Date(), bleacon.uuid, bleacon.major, bleacon.minor,
         bleacon.measuredPower, bleacon.rssi, bleacon.proximity, bleacon.accuracy);
+        socket.emit('deviceData', {mac: bleacon.uuid, major:bleacon.major});    
+
 })
